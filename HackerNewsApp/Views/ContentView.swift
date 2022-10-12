e72c344c017ee671c1b8469cb83f12ff6e4a71d5
Cory Ginsberg <cory.ginsberg1@gmail.com>
2022-10-11 23:44:44 -0500

    Recreated whole app to hopefully fix errors

---
+++ HackerNewsApp/Views/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  Copyright (c) 2022 Cory Ginsberg.
+//
+//  Licensed under the Apache License, Version 2.0
+//
+
+import CoreData
+import SwiftUI
+import FirebaseCore
+import FirebaseDatabaseSwift
+
+struct ContentView: View {
+  @Environment(\.managedObjectContext) private var viewContext
+
+//  @ObservedObject var fetch = HackerNewsAPI()
+  
+  var body: some View {
+    TabView {
+      PostsView()
+        .tabItem {
+          Label("Posts", systemImage: "newspaper.fill")
+        }
+      InboxView()
+        .tabItem {
+          Label("Inbox", systemImage: "envelope.fill")
+        }
+      ProfileView()
+        .tabItem {
+          Label("Profile", systemImage: "person.crop.circle")
+        }
+      SettingsView()
+        .tabItem {
+          Label("Settings", systemImage: "gearshape.fill")
+        }
+    }
+    .accentColor(Color(.systemOrange))
+  }
+}
+
+private let itemFormatter: DateFormatter = {
+  let formatter = DateFormatter()
+  formatter.dateStyle = .short
+  formatter.timeStyle = .medium
+  return formatter
+}()
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ HackerNewsApp/Views/ProfileView.swift
@@ -0,0 +1,31 @@
+//
+//  Copyright (c) 2022 Cory Ginsberg.
+//
+//  Licensed under the Apache License, Version 2.0
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+  let title: String = "My Profile"
+
+  var body: some View {
+    NavigationView {
+      Text("{Username} has {x} karma").font(.title)
+      .navigationTitle(title)
+      .toolbar {
+        ToolbarItemGroup(placement: .navigationBarTrailing) {
+          Button(action: {}) {
+            Text("Logout")
+          }
+        }
+      }
+    }
+  }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
