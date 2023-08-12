7dc2ee2b58774701220f67dc26a7cbefa22505d7
Cory Ginsberg <cory.ginsberg1@gmail.com>
2023-08-12 13:43:08 -0500

    Initial Commit

---
+++ Hermes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hermes
+//
+//  Created by Cory Ginsberg on 8/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hermes/HermesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HermesApp.swift
+//  Hermes
+//
+//  Created by Cory Ginsberg on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HermesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
